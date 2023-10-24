5359649907b746653af97adb8a12bd61ba7feacf
Renan França <renanfranca.contato@hotmail.com>
2023-10-24 18:23:58 -0300

    first commit

---
+++ app/src/main/java/com/example/coveragetestapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.coveragetestapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.coveragetestapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(LayoutInflater.from(this))
+    }
+
+    private var counter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        updateCounter()
+        setupClicks()
+    }
+
+    private fun setupClicks() {
+        binding.incrementButton.setOnClickListener {
+            counter += 1
+            updateCounter()
+        }
+
+        binding.decrementButton.setOnClickListener {
+            counter -= 1
+            updateCounter()
+        }
+
+        binding.navigateToPicPayButton.setOnClickListener {
+            navigateToPicPay()
+        }
+
+        binding.navigateToSecond.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
+    }
+
+    private fun updateCounter() {
+        binding.counterTextView.text = counter.toString()
+    }
+
+    private fun navigateToPicPay() {
+        val intent = packageManager.getLaunchIntentForPackage("com.picpay.debug")
+        startActivity(intent)
+    }
+}
